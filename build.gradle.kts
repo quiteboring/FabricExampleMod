9f360b5705ff8a0c6ee2bcd838f66b7d6b0d0d6c
Nathan <209938737+quiteboring@users.noreply.github.com>
2026-02-04 15:14:30 -0500

    chore: update to mojang dep + 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,11 +27,11 @@ repositories {
 
 dependencies {
   minecraft("com.mojang:minecraft:${property("minecraft_version")}")
-  mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
-  modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+  mappings(loom.officialMojangMappings())
 
+  modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
   modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
-  modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
+  modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
 
   modRuntimeOnly("me.djtheredstoner:DevAuth-fabric:1.2.1")
 }
